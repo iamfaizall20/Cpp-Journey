cbae00168a8b7aed61a44542366cf669f4f6a55f
Faizal Hassan <161876313+iamfaizall20@users.noreply.github.com>
2025-03-01 23:10:30 +0500

    Add files via upload

---
+++ Basics/MinutesIntoSeconds.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int minutes = 1,seconds = 1;
+ 	
+	cout<<"Enter Minutes: ";
+	cin>>minutes;
+
+	seconds = minutes*60;	
+	cout<<"Seconds: "<<seconds;
+}
